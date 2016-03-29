8bad737393a6dc1b1b303d262b4cfda5d915fa41
tiny6996 <tiny515515@gmail.com>
2016-03-29 17:30:51 -0500

    Delete buttonpress.py

--- buttonpress.py
+++
@@ -1,9 +0,0 @@
-import os
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7,GPIO.IN)
-input = GPIO.input(7)
-while True:
-	if(GPIO.input(7)== False):
-		print("every time you push the button you are slowly killing me")
-		os.system('clear')
