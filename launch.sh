5676158bd6259e72711cfd2f4a1f29546ef6cd16
tiny6996 <tiny515515@gmail.com>
2016-04-18 22:40:47 -0500

    Update launch.sh
    updated the file so it runs the main program on boot

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 #This script can be added as a cron job so it starts on boot 
 #move to pythondirectory and start the python sript cal.py on boot
 
-cd /home/pi/python
-python cal.py
+cd /home/pi/nocal
+python main.py
 cd /
 
