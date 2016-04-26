bdbc30bd12a2bf8cdb3879cbb5d7726633895293
Mathias Palmersheim <mathias.palmersheim@loras.edu>
2016-04-26 12:28:22 -0500

    fixed error when calculating tomorrows sunrise

--- main.py
+++ main.py
@@ -109,9 +109,9 @@ while True :
     tomorrow = today + delta
 
     # Find the time for today's sunrise and sunset as well as tomorrow sunrise
-    setTime = ephem.localtime(panels.previous_rising(sun))
+    setTime = ephem.localtime(panels.previous_setting(sun))
     riseTime = ephem.localtime(panels.next_rising(sun))
-    tomorrowRise = dt.datetime.tomorrow(panels.next_rising(sun))
+    tomorrowRise = ephem.localtime(panels.next_rising(sun))
 
     # Using the dates found above calculate the day length and the delay time for the motors
     dayLength = int((setTime - dt.datetime.now().total_seconds()))
